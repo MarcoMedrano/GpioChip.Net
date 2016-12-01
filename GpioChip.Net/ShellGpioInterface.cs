36d7aff7393f120d8d2f607e63a17f331f61fa75
Marco Medrano <marco.medrano.garay@gmail.com>
2016-12-01 15:19:24 -0400

    Added missing file

---
+++ GpioChip.Net/ShellGpioInterface.cs
@@ -0,0 +1,37 @@
+﻿namespace GpioChip.Net
+{
+    using System.Diagnostics;
+
+    public class ShellGpioInterface : AbstractShellGpioInterface
+    {
+        private readonly string username;
+
+        public ShellGpioInterface(string username = "root", string password = "chip") : base()
+        {
+            this.username = username;
+        }
+
+        protected override AbstractShellCommandResult RunCommand(string command)
+        {
+            var p = new Process();
+            p.StartInfo = new ProcessStartInfo("sh", $"-c '{command}'");
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.Start();
+
+            string res = p.StandardOutput.ReadToEnd();
+            while (!p.HasExited)
+            {
+                res += p.StandardOutput.ReadToEnd();
+            }
+
+            return new AbstractShellCommandResult { Result = res, ExitCode = p.ExitCode };
+            return new AbstractShellCommandResult {};
+        }
+
+        public new void Dispose()
+        {
+            base.Dispose();
+        }
+    }
+}
--- tests/ConsoleTest/Program.cs
+++ tests/ConsoleTest/Program.cs
@@ -2,23 +2,25 @@
 
 namespace GpioChip.Net.tests.ConsoleTest
 {
-    using System.Security.Cryptography.X509Certificates;
+    using System.Diagnostics;
     using System.Threading;
 
     public class Program
     {
         public static void Main(string[] args)
         {
+            var stopWatch = Stopwatch.StartNew();
             Console.WriteLine("Running GPIO");
             TestOutput();
             //TestInput();
-            Console.WriteLine("Finished");
+            Console.WriteLine($"Finished with: {stopWatch.Elapsed.TotalSeconds} seconds");
             Console.ReadKey();
         }
 
         private static void TestOutput()
         {
-            using (var gpio = new Gpio(new SshGpioInterface("192.168.0.109")))
+            using (var gpio = new Gpio(new ShellGpioInterface()))
+           // using (var gpio = new Gpio(new SshGpioInterface("192.168.0.109")))
             {
                 Console.WriteLine("Connected");
                 for (short i = 0; i < 8; i++)
@@ -27,6 +29,14 @@ namespace GpioChip.Net.tests.ConsoleTest
                     pin.On();
                     pin.Off();
                 }
+
+                //var lcd4Pin = gpio.NewOutputPin(2, PinBase.LCD_D2);
+                //lcd4Pin.On();
+                //lcd4Pin.Off();
+
+                var lcd4Pin = gpio.NewOutputPin(3, PinBase.CSIPCK);
+                lcd4Pin.On();
+                lcd4Pin.Off();
             }
         }
 
